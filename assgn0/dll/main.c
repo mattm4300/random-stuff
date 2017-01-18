43d4d3322ef0e23bbd1ef4d7a3b62815dae2184b
MattM <mattm4300@gmail.com>
2017-01-17 21:42:42 -0600

    working on dll

--- assgn0/dll/main.c
+++ assgn0/dll/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include "dll.h"
+#include "integer.h"
+
+void
+Fatal(char *fmt, ...)
+    {
+    va_list ap;
+
+    fprintf(stderr,"An error occured: ");
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+
+    exit(-1);
+    }
 
 int main(void) {
-
+    dll *mydll = newDLL(displayInteger);
+    free(mydll);
+    printf("done.\n");
     return 0;
 }
