2b4dc9260255c8da2f4a13b40dfdf65ecbe1fd84
MattM <mattm4300@gmail.com>
2017-01-18 17:19:34 -0600

    testing stack

--- assgn0/stack/main.c
+++ assgn0/stack/main.c
@@ -27,22 +27,31 @@ static void showStack(stack *s)
 int main(void) {
      stack *items = newStack(displayInteger);
        showStack(items);
+       printf("The stack size is: %d\n", sizeStack(items));
        push(items,newInteger(3));
+       showStack(items);
+       printf("The stack size is: %d\n", sizeStack(items));
        push(items,newInteger(2));
        showStack(items);
+       printf("The stack size is: %d\n", sizeStack(items));
        printf("The value ");
        displayInteger(stdout,pop(items));
        printf(" was popped.\n");
        showStack(items);
+       printf("The stack size is: %d\n", sizeStack(items));
        printf("The value ");
        displayInteger(stdout, pop(items));
        printf(" was popped.\n");
        showStack(items);
+       printf("The stack size is: %d\n", sizeStack(items));
        push(items, newInteger(22));
        showStack(items);
+       printf("The stack size is: %d\n", sizeStack(items));
        pop(items);
+       showStack(items);
+       printf("The stack size is: %d\n", sizeStack(items));
        push(items, newInteger(25));
        showStack(items);
+       printf("The stack size is: %d\n", sizeStack(items));
        return 0;
-    return 0;
 }
