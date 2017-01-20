8c7c81b6805fae2aee2d7650c33d1854e2fa7111
MattM <mattm4300@gmail.com>
2017-01-19 22:42:54 -0600

    trying to get the makefile ok

---
+++ assgn0/testdir/test-stack.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <time.h>
+#include "stack.h"
+#include "integer.h"
+
+void
+Fatal(char *fmt, ...)
+    {
+    va_list ap;
+
+    fprintf(stderr,"An error occured: ");
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+
+    exit(-1);
+    }
+
+int main(void) {
+
+     return 0;
+}
