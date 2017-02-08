f443ee81e5b9517e927de521ac222efebb8f7ec1
Matt_M <mattm4300@gmail.com>
2017-02-08 14:59:12 -0600

    assgn1

--- assgn1/testdir/reader.c
+++ assgn1/testdir/reader.c
@@ -1,7 +1,43 @@
+/* =======================================================================
+ * FILENAME: reader.c
+ *
+ * AUTHOR: Matthew Morgan, mdmorgan1@crimson.ua.edu
+ *
+ * DESCRIPTION:
+ *   Contains implementations for integer *, real *, and string *
+ *   reader functions.
+ *
+ * PUBLIC FUNCTIONS:
+ *   void readIntegers(FILE *, queue *)
+ *   void readReals(FILE *, queue *)
+ *   void readStrings(FILE *, queue *)
+ *
+ *
+ * NOTES:
+ *   The associated header file for this file is "reader.h".
+ *   These functions use the scanner functions defined and published
+ *   for free use by Dr. Lusth, University of Alabama.
+ *
+ * VERSION 1.0
+ * =======================================================================
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "reader.h"
 
+
+/* ============================================================================
+ * readIntegers
+ * ----------------------------------------------------------------------------
+ * Reads in and adds integers to a queue indefinitely, until EOF is read.
+ *
+ * fp: The file pointer to read from.
+ * q: The queue to which add values.
+ *
+ * returns: void
+ * ============================================================================
+*/
 void readIntegers(FILE *fp, queue *q) {
      int x = readInt(fp);
      while(!feof(fp)) {
@@ -10,6 +46,17 @@ void readIntegers(FILE *fp, queue *q) {
      }
 }
 
+/* ============================================================================
+ * readReals
+ * ----------------------------------------------------------------------------
+ * Reads in and adds reals to a queue indefinitely, until EOF is read.
+ *
+ * fp: The file pointer to read from.
+ * q: The queue to which add values.
+ *
+ * returns: void
+ * ============================================================================
+*/
 void readReals(FILE *fp, queue  *q) {
      double x = readReal(fp);
      while(!feof(fp)) {
@@ -18,6 +65,17 @@ void readReals(FILE *fp, queue  *q) {
      }
 }
 
+/* ============================================================================
+ * readStrings
+ * ----------------------------------------------------------------------------
+ * Reads in and adds strings to a queue indefinitely, until EOF is read.
+ *
+ * fp: The file pointer to read from.
+ * q: The queue to which add values.
+ *
+ * returns: void
+ * ============================================================================
+*/
 void readStrings(FILE *fp, queue *q) {
      char *x = readString(fp);
      while(!feof(fp)) {
--- assgn1/testdir/reader.h
+++ assgn1/testdir/reader.h
@@ -1,3 +1,20 @@
+/* =======================================================================
+ * FILENAME: reader.h
+ *
+ * AUTHOR: Matthew Morgan, mdmorgan1@crimson.ua.edu
+ *
+ * DESCRIPTION:
+ *   Header file for integer *, real *, and string * reader functions.
+ *
+ * NOTES:
+ *   The associated implementation file for this file is "reader.c".
+ *   These reader functions use the scanner functions published for
+ *   free use by Dr. Lusth, University of Alabama.
+ *
+ * VERSION: 1.0
+ * =======================================================================
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "integer.h"
@@ -9,10 +26,12 @@
 #ifndef _READER_H_
 #define _READER_H_
 
+// Reader function pointer.
 typedef void (*Reader)(FILE *, queue *);
 
 Reader read;
 
+// Function definitions.
 extern void readIntegers(FILE *, queue *);
 extern void readReals(FILE *, queue *);
 extern void readStrings(FILE *, queue *);
