2a3bdbb023655b3b258d4d86b071b4c26ddc07b9
MattM <mattm4300@gmail.com>
2017-03-20 14:19:30 -0500

    Added binomial modules.

---
+++ assgn3/binomial.h
@@ -0,0 +1,21 @@
+#ifndef __BINHEAP_INCLUDED__
+#define __BINHEAP_INCLUDED__
+
+#include <stdio.h>
+#include "darray.h"
+
+//typedef struct BinomialNode BinomialNode;
+
+BinomialNode *newBinomialNode(void (*display)(FILE *,void *), void *value);
+void displayBinomialNode(FILE *fp,void *n);
+
+typedef struct Binomial Binomial;
+
+extern Binomial *newBinomial(void (*d)(FILE *,void *), int (*c)(void *,void *), void (*u)(void *,BinomialNode *));
+extern BinomialNode *insertBinomial(Binomial *b,void *value);
+extern int sizeBinomial(Binomial *b);
+extern void deleteBinomial(Binomial *b,BinomialNode *n);
+extern void decreaseKeyBinomial(Binomial *b,BinomialNode *n,void *value);
+extern void *extractBinomial(Binomial *b);
+extern void displayBinomial(FILE *fp,Binomial *b);
+#endif
