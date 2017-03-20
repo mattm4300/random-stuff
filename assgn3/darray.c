b809f7cf20b4b7f09673a863b80969b636168095
MattM <mattm4300@gmail.com>
2017-03-20 17:59:43 -0500

    Added the free back into the setDArray function b/c with it out it causes a memory leak.

--- assgn3/darray.c
+++ assgn3/darray.c
@@ -51,6 +51,7 @@ void setDArray(DArray *a, int index, void *value) {
      // If index is the size of the array, call insertDArray instead.
      if(index == a->size) return insertDArray(a, value);
      else {
+          // Make sure to free the old value in memory or memory leak WILL occur.
           free(a->array[index]);
           a->array[index] = value;
      }
