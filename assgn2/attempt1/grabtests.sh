40f7862bec60649d702b2803a3c5a0c864462faa
Matt_M <mattm4300@gmail.com>
2017-03-21 10:28:37 -0500

    Added Brandon's test files.

--- assgn2/attempt1/grabtests.sh
+++ assgn2/attempt1/grabtests.sh
@@ -1,39 +1,40 @@
 echo Downloading Assignment 2 Test Files...
 
-wget beastie.cs.ua.edu/cs201/assign2/display
-wget beastie.cs.ua.edu/cs201/assign2/spaces.0
-wget beastie.cs.ua.edu/cs201/assign2/empty
-wget beastie.cs.ua.edu/cs201/assign2/commands.0
-wget beastie.cs.ua.edu/cs201/assign2/s9.0
-wget beastie.cs.ua.edu/cs201/assign2/s10.0
-wget beastie.cs.ua.edu/cs201/assign2/s11.0
-wget beastie.cs.ua.edu/cs201/assign2/s12.0
-wget beastie.cs.ua.edu/cs201/assign2/s13.0
-wget beastie.cs.ua.edu/cs201/assign2/s14.0
-wget beastie.cs.ua.edu/cs201/assign2/s15.0
-wget beastie.cs.ua.edu/cs201/assign2/w16.0
-wget beastie.cs.ua.edu/cs201/assign2/w17.0
-wget beastie.cs.ua.edu/cs201/assign2/w18.0
-wget beastie.cs.ua.edu/cs201/assign2/w19.0
-wget beastie.cs.ua.edu/cs201/assign2/w20.0
-wget beastie.cs.ua.edu/cs201/assign2/w21.0
-wget beastie.cs.ua.edu/cs201/assign2/w22.0
-wget beastie.cs.ua.edu/cs201/assign2/w23.0
-wget beastie.cs.ua.edu/cs201/assign2/w24.0
-wget beastie.cs.ua.edu/cs201/assign2/c25.0
-wget beastie.cs.ua.edu/cs201/assign2/c26.0
-wget beastie.cs.ua.edu/cs201/assign2/c27.0
-wget beastie.cs.ua.edu/cs201/assign2/c28.0
-wget beastie.cs.ua.edu/cs201/assign2/c29.0
-wget beastie.cs.ua.edu/cs201/assign2/c30.0
-wget beastie.cs.ua.edu/cs201/assign2/c31.0
-wget beastie.cs.ua.edu/cs201/assign2/c32.0
-wget beastie.cs.ua.edu/cs201/assign2/shakespeare.txt
-wget beastie.cs.ua.edu/cs201/assign2/shakespeare.display
-wget beastie.cs.ua.edu/cs201/assign2/beowulf.txt
-wget beastie.cs.ua.edu/cs201/assign2/c35.0
-wget beastie.cs.ua.edu/cs201/assign2/c36.0
-wget beastie.cs.ua.edu/cs201/assign2/c37.0
+curl -O beastie.cs.ua.edu/cs201/assign2/clean.0
+curl -O beastie.cs.ua.edu/cs201/assign2/display
+curl -O beastie.cs.ua.edu/cs201/assign2/spaces.0
+curl -O beastie.cs.ua.edu/cs201/assign2/empty
+curl -O beastie.cs.ua.edu/cs201/assign2/commands.0
+curl -O beastie.cs.ua.edu/cs201/assign2/s9.0
+curl -O beastie.cs.ua.edu/cs201/assign2/s10.0
+curl -O beastie.cs.ua.edu/cs201/assign2/s11.0
+curl -O beastie.cs.ua.edu/cs201/assign2/s12.0
+curl -O beastie.cs.ua.edu/cs201/assign2/s13.0
+curl -O beastie.cs.ua.edu/cs201/assign2/s14.0
+curl -O beastie.cs.ua.edu/cs201/assign2/s15.0
+curl -O beastie.cs.ua.edu/cs201/assign2/w16.0
+curl -O beastie.cs.ua.edu/cs201/assign2/w17.0
+curl -O beastie.cs.ua.edu/cs201/assign2/w18.0
+curl -O beastie.cs.ua.edu/cs201/assign2/w19.0
+curl -O beastie.cs.ua.edu/cs201/assign2/w20.0
+curl -O beastie.cs.ua.edu/cs201/assign2/w21.0
+curl -O beastie.cs.ua.edu/cs201/assign2/w22.0
+curl -O beastie.cs.ua.edu/cs201/assign2/w23.0
+curl -O beastie.cs.ua.edu/cs201/assign2/w24.0
+curl -O beastie.cs.ua.edu/cs201/assign2/c25.0
+curl -O beastie.cs.ua.edu/cs201/assign2/c26.0
+curl -O beastie.cs.ua.edu/cs201/assign2/c27.0
+curl -O beastie.cs.ua.edu/cs201/assign2/c28.0
+curl -O beastie.cs.ua.edu/cs201/assign2/c29.0
+curl -O beastie.cs.ua.edu/cs201/assign2/c30.0
+curl -O beastie.cs.ua.edu/cs201/assign2/c31.0
+curl -O beastie.cs.ua.edu/cs201/assign2/c32.0
+curl -O beastie.cs.ua.edu/cs201/assign2/shakespeare.txt
+curl -O beastie.cs.ua.edu/cs201/assign2/shakespeare.display
+curl -O beastie.cs.ua.edu/cs201/assign2/beowulf.txt
+curl -O beastie.cs.ua.edu/cs201/assign2/c35.0
+curl -O beastie.cs.ua.edu/cs201/assign2/c36.0
+curl -O beastie.cs.ua.edu/cs201/assign2/c37.0
 
 echo Download Complete.
 echo Displaying directory:
