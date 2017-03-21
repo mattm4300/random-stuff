aeb69da20da17c915f36afc7fefb9657ffd56b9b
Matt_M <mattm4300@gmail.com>
2017-03-21 08:59:32 -0500

    Starting to understand the display outline for the project.

--- assgn3/binomial.c
+++ assgn3/binomial.c
@@ -25,15 +25,22 @@ BinomialNode *newBinomialNode(void (*display)(FILE *,void *), void *value) {
      return n;
 }
 
+void displayBinomialNode(FILE *fp,void *n) {
+     BinomialNode *x = (BinomialNode *) n; // "Unbox" the value.
+     x->display(fp, x->value);
+     fprintf(fp, "-N"); // Add order sometime.
+     x->display(fp )
+}
+
 Binomial *newBinomial(
           void (*d)(FILE *,void *), // Display
           int (*c)(void *,void *), // Comparator
           void (*u)(void *,BinomialNode *)) { // Updater
      Binomial *b = malloc(sizeof(Binomial));
-     b->display = d;
+     b->display = d; // This will be something like displayInteger!
      b->compare = c;
      b->update = u;
-     b->rootlist = newDArray(d);
+     b->rootlist = newDArray(displayBinomialNode); // I think this is right!
      b->extreme = NULL;
      b->size = 0;
      return b;
@@ -41,6 +48,7 @@ Binomial *newBinomial(
 
 BinomialNode *insertBinomial(Binomial *b, void *value) {
      BinomialNode *n = newBinomialNode(value, b->display);
+
 }
 
 int sizeBinomial(Binomial *b) { return b->size; }
